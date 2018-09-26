fc487a3e8fa2fd34fd064fb90b442696df94a015
zurda <m.weizman@gmail.com>
2018-09-26 19:30:21 +0100

    Updates

--- App.0e534735.js
+++ App.0e534735.js
@@ -22889,7 +22889,7 @@ var api = {
 };
 
 exports.default = api;
-},{"axios":"../node_modules/axios/index.js"}],"DisplayUsersList.js":[function(require,module,exports) {
+},{"axios":"../node_modules/axios/index.js"}],"User.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -22901,6 +22901,162 @@ var _createClass = function () { function defineProperties(target, props) { for 
 var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var User = function (_React$Component) {
+  _inherits(User, _React$Component);
+
+  function User() {
+    _classCallCheck(this, User);
+
+    return _possibleConstructorReturn(this, (User.__proto__ || Object.getPrototypeOf(User)).apply(this, arguments));
+  }
+
+  _createClass(User, [{
+    key: "render",
+    value: function render() {
+      return _react2.default.createElement(
+        "div",
+        {
+          className: "item",
+          id: this.props.username,
+          key: this.props.id,
+          style: { border: "none" }
+        },
+        _react2.default.createElement(
+          "div",
+          { className: "image" },
+          _react2.default.createElement("img", {
+            src: this.props.avatarUrl,
+            alt: "User avatar",
+            style: { borderRadius: "5px" }
+          })
+        ),
+        _react2.default.createElement(
+          "div",
+          { className: "left aligned content" },
+          _react2.default.createElement(
+            "div",
+            { className: "header" },
+            _react2.default.createElement(
+              "a",
+              {
+                style: { cursor: "default" },
+                href: "https://github.com/" + this.props.username
+              },
+              _react2.default.createElement("i", { className: "star icon", title: "Stargazers" })
+            ),
+            _react2.default.createElement(
+              "span",
+              null,
+              this.props.stargazers
+            ),
+            _react2.default.createElement(
+              "a",
+              {
+                href: "https://github.com/" + this.props.username + "?tab=followers",
+                target: "_blank",
+                rel: "noopener noreferrer"
+              },
+              _react2.default.createElement("i", { className: "users icon", title: "Followers" })
+            ),
+            _react2.default.createElement(
+              "span",
+              null,
+              this.props.followers
+            ),
+            _react2.default.createElement(
+              "a",
+              {
+                href: "https://github.com/" + this.props.username + "?tab=repositories",
+                target: "_blank",
+                rel: "noopener noreferrer"
+              },
+              _react2.default.createElement("i", { className: "file code outline icon", title: "Repositories" })
+            ),
+            _react2.default.createElement(
+              "span",
+              null,
+              this.props.repos
+            ),
+            _react2.default.createElement("i", {
+              className: "icon delete ui right",
+              onClick: this.props.click,
+              style: { color: "lightgrey" },
+              title: "Remove from list"
+            })
+          ),
+          _react2.default.createElement(
+            "div",
+            { className: "description" },
+            _react2.default.createElement(
+              "h4",
+              null,
+              this.props.title,
+              _react2.default.createElement("br", null),
+              _react2.default.createElement(
+                "a",
+                {
+                  href: "https://github.com/" + this.props.username,
+                  target: "_blank",
+                  rel: "noopener noreferrer"
+                },
+                this.props.username
+              )
+            ),
+            _react2.default.createElement(
+              "p",
+              null,
+              this.props.bio
+            )
+          ),
+          _react2.default.createElement(
+            "div",
+            { className: "extra" },
+            _react2.default.createElement(
+              "span",
+              null,
+              "Location:"
+            ),
+            this.props.location,
+            _react2.default.createElement(
+              "span",
+              { style: { display: "block", color: "green" } },
+              this.props.hireable ? "Availble for job offers" : null
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return User;
+}(_react2.default.Component);
+
+exports.default = User;
+},{"react":"../node_modules/react/index.js"}],"DisplayUsersList.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _User = require("./User");
+
+var _User2 = _interopRequireDefault(_User);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -22937,7 +23093,7 @@ var DisplayUsersList = function (_React$Component) {
           return b[sortBy] - a[sortBy];
         });
         userComponents = users.map(function (user) {
-          return _react2.default.createElement(User, {
+          return _react2.default.createElement(_User2.default, {
             key: user.id,
             id: user.id,
             title: user.title,
@@ -22966,132 +23122,8 @@ var DisplayUsersList = function (_React$Component) {
   return DisplayUsersList;
 }(_react2.default.Component);
 
-var User = function (_React$Component2) {
-  _inherits(User, _React$Component2);
-
-  function User() {
-    _classCallCheck(this, User);
-
-    return _possibleConstructorReturn(this, (User.__proto__ || Object.getPrototypeOf(User)).apply(this, arguments));
-  }
-
-  _createClass(User, [{
-    key: "render",
-    value: function render() {
-      return _react2.default.createElement(
-        "div",
-        {
-          className: "item",
-          id: this.props.username,
-          key: this.props.id,
-          style: { border: "none" }
-        },
-        _react2.default.createElement(
-          "div",
-          { className: "image" },
-          _react2.default.createElement("img", {
-            src: this.props.avatarUrl,
-            alt: "User avatar",
-            style: { borderRadius: "5px" }
-          })
-        ),
-        _react2.default.createElement(
-          "div",
-          { className: "left aligned content" },
-          _react2.default.createElement(
-            "div",
-            { className: "header" },
-            _react2.default.createElement(
-              "a",
-              { style: { cursor: "default" } },
-              _react2.default.createElement("i", { className: "star icon", title: "Stargazers" })
-            ),
-            _react2.default.createElement(
-              "span",
-              null,
-              this.props.stargazers
-            ),
-            _react2.default.createElement(
-              "a",
-              {
-                href: "https://github.com/" + this.props.username + "?tab=followers",
-                target: "_blank"
-              },
-              _react2.default.createElement("i", { className: "users icon", title: "Followers" })
-            ),
-            _react2.default.createElement(
-              "span",
-              null,
-              this.props.followers
-            ),
-            _react2.default.createElement(
-              "a",
-              {
-                href: "https://github.com/" + this.props.username + "?tab=repositories",
-                target: "_blank"
-              },
-              _react2.default.createElement("i", { className: "file code outline icon", title: "Repositories" })
-            ),
-            _react2.default.createElement(
-              "span",
-              null,
-              this.props.repos
-            ),
-            _react2.default.createElement("i", {
-              className: "icon delete ui right",
-              onClick: this.props.click,
-              style: { color: "lightgrey" },
-              title: "Remove from list"
-            })
-          ),
-          _react2.default.createElement(
-            "div",
-            { className: "description" },
-            _react2.default.createElement(
-              "h4",
-              null,
-              this.props.title,
-              _react2.default.createElement("br", null),
-              _react2.default.createElement(
-                "a",
-                {
-                  href: "https://github.com/" + this.props.username,
-                  target: "_blank"
-                },
-                this.props.username
-              )
-            ),
-            _react2.default.createElement(
-              "p",
-              null,
-              this.props.bio
-            )
-          ),
-          _react2.default.createElement(
-            "div",
-            { className: "extra" },
-            _react2.default.createElement(
-              "span",
-              null,
-              "Location:"
-            ),
-            this.props.location,
-            _react2.default.createElement(
-              "span",
-              { style: { display: "block", color: "green" } },
-              this.props.hireable ? "Availble for job offers" : null
-            )
-          )
-        )
-      );
-    }
-  }]);
-
-  return User;
-}(_react2.default.Component);
-
 exports.default = DisplayUsersList;
-},{"react":"../node_modules/react/index.js"}],"Main.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","./User":"User.js"}],"Main.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -23525,7 +23557,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57927' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62926' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
